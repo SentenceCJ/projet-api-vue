915fd681f6180be3a1d5f5ca17a14951e1c74885
Antoine <antoine.macheda@ynov.com>
2023-10-12 08:31:56 +0200

    First commit Projet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Interface Utilisateur</h1>
+    <label for="day">Jour :</label>
+    <input v-model="day" type="text" id="day" />
+    <label for="month">Mois :</label>
+    <input v-model="month" type="text" id="month" />
+    <label for="year">Année :</label>
+    <input v-model="year" type="text" id="year" />
+    <button @click="generateUI">Générer UI</button>
+    <div v-if="uiData">
+      <h2>Résultats</h2>
+      <p>Jour : {{ uiData.day }}</p>
+      <p>Mois : {{ uiData.month }}</p>
+      <p>Année : {{ uiData.year }}</p>
+      <h2>Date sélectionnée : {{ uiData.selectedDate }}</h2>
+      <!-- Affichez d'autres données ici -->
+    </div>
+    <div v-if="showCalendar">
+      <label>Sélectionnez une date :</label>
+      <DatePicker :value="selectedDate" format="yyyy-MM-dd" @input="updateSelectedDate"></DatePicker>
+    </div>
   </div>
 </template>
 
 <script>
+import DatePicker from 'vue3-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    DatePicker,
+  },
+  data() {
+    return {
+      day: '',
+      month: '',
+      year: '',
+      uiData: null,
+      selectedDate: '', // Initialisez selectedDate à vide
+      showCalendar: false,
+    };
+  },
+  methods: {
+    async generateUI() {
+      try {
+        const response = await fetch('http://localhost:3000/api/generate-ui', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            day: this.day,
+            month: this.month,
+            year: this.year,
+          }),
+        });
+        if (response.ok) {
+          this.uiData = await response.json();
+          this.selectedDate = `${this.uiData.year}-${this.uiData.month.padStart(2, '0')}-${this.uiData.day.padStart(2, '0')}`;
+          this.showCalendar = true;
+        }
+      } catch (error) {
+        console.error('Une erreur s\'est produite :', error);
+      }
+    },
+    updateSelectedDate(newDate) {
+      this.selectedDate = newDate;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+/* Ajoutez des styles CSS ici si nécessaire */
 </style>
